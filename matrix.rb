7202d7a25f04b7684744f98030626f8941e0f115
helmihidzir <helmihidzir@gmail.com>
2020-08-09 11:26:42 +0800

    Refactor

--- matrix.rb
+++ matrix.rb
@@ -1,15 +1,15 @@
 class Matrix
-  attr_accessor :array_numbers
+  attr_reader :array_numbers
 
   def initialize(array_numbers)
     @array_numbers = array_numbers
   end
 
   def rows
-    array_numbers.split(/\n/).map { |row| row.split(' ').map { |row| row.to_i  } }
+    array_numbers.each_line.map { |row| row.split.map { |row| row.to_i  } }
   end
 
   def columns
-    array_numbers.split(/\n/).map { |row| row.split(' ').map { |row| row.to_i  } }.transpose
+    rows.transpose
   end
 end
